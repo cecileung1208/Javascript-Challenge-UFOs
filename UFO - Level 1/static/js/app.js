62745e0fd56f3981e47266647dc109865dfbf283
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-12 13:49:30 -0500

    update entry from resfreshing

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -28,9 +28,18 @@ tableData.forEach((ufo) => {
 //FILTER
 
 var button = d3.select("#filter-btn");
+var form = d3.select("#form");
 
 // Create event handlers 
-button.on("click", function(){
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
   // Select the input element and get the HTML node
   var inputElement = d3.select("#datetime");
@@ -43,10 +52,10 @@ button.on("click", function(){
 
 
   //see if output results are captured
-  console.log(inputValue)
-  console.log(filteredData)
+  console.log(inputValue);
+  console.log(filteredData);
 
-});
+};
 
 
 
