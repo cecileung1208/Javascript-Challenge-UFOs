8f251146ef19732193c081d238d52c78ec249513
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-12 11:22:23 -0500

    insert data

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select tbody - this is where to input the UFO data into the table
+var tbody = d3.select("tbody");
+
+//Loop Through `data` and console.log each weather report object
+tableData.forEach((ufo) => {
+
+    //Append Rows to table
+    var row = tbody.append("tr");
+
+    //Insert the tableData Array into the table
+    Object.entries(ufo).forEach(([key, value]) => {
+
+        //Add cells to each row
+        var cell = row.append("td");
+        
+        //Add value to each cell
+        cell.text(value);
+
+    });
+
+});
