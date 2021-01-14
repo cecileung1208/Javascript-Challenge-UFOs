18aa195a7d0f838b9ca4556c17f257c014363c20
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-13 21:01:22 -0500

    add comments

--- UFO - Level 2/static/js/app.js
+++ UFO - Level 2/static/js/app.js
@@ -1,16 +1,15 @@
 // from data.js
 var tableData = data;
 
-var tbody = d3.select("tbody");
+
 
 //TABLE
 
 // select tbody - this is where to input the UFO data into the table
+var tbody = d3.select("tbody");
 
 
-
-// //Loop Through `data` and console.log each weather report object
-
+//Loop Through `data` and console.log each weather report object
 
 tableData.forEach((ufo) => {
 
@@ -32,9 +31,11 @@ tableData.forEach((ufo) => {
 
 // MULTIPLE FILTERS 
 
+//Select Button and Set Event
 var button = d3.select("#filter-btn");
 button.on("click", function() {
 
+  // empty existing table before we input the new information
   tbody.html("");
 
   // Select the input element and get the HTML node
@@ -42,20 +43,32 @@ button.on("click", function() {
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
+
+  // filter data from using date, city, state, country and shape inputs from dropdown
   var filteredData = tableData.filter(entry => entry.datetime === inputValue ||
                                                entry.city === inputValue ||
                                                entry.state === inputValue ||
                                                entry.country === inputValue ||
                                                entry.shape === inputValue);
 
-  
+  //see if output results are captured
+  //console.log(inputValue);
+  //console.log(filteredData);
 
+  //Loop through Filtered Data
   filteredData.forEach((ufo) => {
-  var row = tbody.append("tr");
-  Object.entries(ufo).forEach(([key, value]) => {
+    //Append Rows to table
+    var row = tbody.append("tr");
+
+    //Insert the tableData Array into the table
+    Object.entries(ufo).forEach(([key, value]) => {
+
+    //Add cells to each row
     var cell = row.append("td");
+
+    //Add value to each cell
     cell.text(value);
-  });
+    });
   });
   
 });
