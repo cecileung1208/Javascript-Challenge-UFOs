4bc455928fd683bd40a97354e230afaf50962540
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-01-13 19:15:17 -0500

    update level 2

--- UFO - Level 2/static/js/app.js
+++ UFO - Level 2/static/js/app.js
@@ -1,21 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
 //TABLE
 
 // select tbody - this is where to input the UFO data into the table
 var tbody = d3.select("tbody");
 
-//Loop Through `data` and console.log each weather report object
-tableData.forEach((ufo) => {
+function ufoTable(data) {
+// //Loop Through `data` and console.log each weather report object
+
+tbody.html("");
+
+data.forEach((ufo) => {
 
     //Append Rows to table
     var row = tbody.append("tr");
 
     //Insert the tableData Array into the table
-    Object.entries(ufo).forEach(([key, value]) => {
+    Object.entries(ufo).forEach((value) => {
 
         //Add cells to each row
         var cell = row.append("td");
@@ -26,3 +28,98 @@ tableData.forEach((ufo) => {
     });
 
 });
+}
+// MULTIPLE FILTERS 
+
+var filters = {};
+
+function filterUpdate() {
+    var thisElement = d3.select(this).select("input");
+    var elementValue = thisElement.property("value");
+    var filterID = thisElement.attr("id");
+
+    if (elementValue){
+      filters[filterID] = elementValue;
+    }
+    else{
+      delete filters[filterID];
+    }
+  filterTable();
+}
+
+function filterTable() {
+  let filterData = tableData;
+  Object.entries(filters).forEach(([key, value]) => {
+  filterData = filterData.filter(row => row[key] === value );
+  
+});
+  ufoTable(filterData);
+};
+
+d3.selectAll(".filter").on(change, filterUpdate);
+
+ufoTable(tableData);
+
+
+// //FILTER
+
+// var button = d3.select("#filter-btn");
+// var form = d3.select("#form");
+
+// // Create event handlers 
+
+// button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+// // Complete the event handler function for the form
+// function runEnter() {
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+
+//   // Select the input element and get the HTML node
+//   var inputElement = d3.select("#datetime");
+
+//   // Get the value property of the input element
+//   var inputValue = inputElement.property("value");
+
+//   // filter data from the date's input
+//   var filteredData = tableData.filter(date => date.datetime=== inputValue);
+
+
+//   //see if output results are captured
+//   //console.log(inputValue);
+//   //console.log(filteredData);
+
+//   // Display Filtered information after inputing the date
+
+//     // select tbody - this is where to input the UFO filtered table
+//     var list = tbody;
+
+//     // empty existing table before we input the new information
+//     list.html("");
+
+//     // 
+//     filteredData.forEach((ufo) => {
+//     //Append Rows to table
+//     var row = tbody.append("tr");
+
+//     //Insert the tableData Array into the table
+//     Object.entries(ufo).forEach(([key, value]) => {
+
+//         //Add cells to each row
+//         var cell = row.append("td");
+        
+//         //Add value to each cell
+//         cell.text(value);
+
+//     });
+
+//   });
+// };
+
+
+
+
+
+ 
